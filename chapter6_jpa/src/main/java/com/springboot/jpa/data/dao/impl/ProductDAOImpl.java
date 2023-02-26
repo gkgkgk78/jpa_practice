c6a2b69ed4ff2d36a7a09efdb8ec64bad884870e
정우진 <woojin1home@naver.com>
2023-02-26 18:33:00 +0900

    tt

--- chapter6_jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
+++ chapter6_jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -3,6 +3,7 @@ package com.springboot.jpa.data.dao.impl;
 import com.springboot.jpa.data.dao.ProductDAO;
 import com.springboot.jpa.data.entity.Product;
 import com.springboot.jpa.data.repository.ProductRepository;
+
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -49,6 +50,7 @@ public class ProductDAOImpl implements ProductDAO {
             product.setUpdatedAt(LocalDateTime.now());
 
             updatedProduct = productRepository.save(product);
+
         } else {
             throw new Exception();
         }
